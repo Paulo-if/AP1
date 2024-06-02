34bb90efc3751e3a626ab00bd3a2592a194ac43f
paulocosta <167147870+Paulo-if@users.noreply.github.com>
2024-06-01 21:42:38 -0300

    Update Número ímpar ou par.c

--- List-03/Número ímpar ou par.c
+++ List-03/Número ímpar ou par.c
@@ -1,4 +1,4 @@
-/*#include <stdio.h>
+#include <stdio.h>
 
 int main() {
     int idade;
@@ -11,29 +11,6 @@ int main() {
     scanf(" %c", &habilitacao); // espaço em %c para descartar valores vazios 
 
     if (idade >= 18 && habilitacao == 's') {
-        printf("Pode dirigir.\n");
-    } else {
-        printf("Nao pode dirigir.\n");
-    }
-
-    return 0;
-}*/
-
-#include <stdio.h>
-#include <string.h> // Para usar a função strcmp()
-
-int main() {
-    int idade;
-    char habilitacao[4]; // Armazena "sim" e o caractere nulo de terminação
-
-    printf("Digite sua idade: ");
-    scanf("%d", &idade);
-
-    printf("Digite 'sim' se voce tem uma habilitacao valida: ");
-    scanf("%s", habilitacao); // Lê a string "sim"
-
-    // Compara a string lida com "sim"
-    if (idade >= 18 && strcmp(habilitacao, "sim") == 0) {
         printf("Pode dirigir.\n");
     } else {
         printf("Nao pode dirigir.\n");
